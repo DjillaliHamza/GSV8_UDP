618483b868ba591ca0316b24942b21096cb37a03
DjillaliHamza <117483864+DjillaliHamza@users.noreply.github.com>
2025-05-19 15:00:41 +0200

    Update Acquisitio_UDP.m

--- Acquisitio_UDP.m
+++ Acquisitio_UDP.m
@@ -4,7 +4,7 @@ clc
 
 %% Load the Library
 if ~libisloaded('MEGSV86x64') 
-    loadlibrary('.\MEGSV86x64.dll', '.\MEGSV86x64.h');
+    loadlibrary('.\\MEGSV86x64.dll', '.\\MEGSV86x64.h');
 end
 
 %% COM Port Configuration
@@ -17,17 +17,19 @@ calllib('MEGSV86x64', 'GSV86setFrequency', com, freq);
 calllib('MEGSV86x64', 'GSV86setZero', com, int32(0));
 calllib('MEGSV86x64', 'GSV86clearDLLbuffer', com);
 
-% %% Setup UDP Sender
+%% Setup UDP Sender
 u = udpport("datagram", "IPV4");
 targetIP = "127.0.0.1";
 targetPort = 25000;
 
-%% Allocate Buffer for GSV86readMultiple
+%% Allocate Buffer for GSV86readMultiple and for error Msg
 count = int32(48000);
 buffer = zeros(1, double(count));
 out = libpointer('doublePtr', buffer);
 valsread = libpointer('int32Ptr', 0);
 errFlags = libpointer('int32Ptr', 0);
+errMsg = blanks(256);
+errTextPtr = libpointer('cstring', errMsg);
 
 %% Initialize Data Storage
 A = [];
@@ -59,18 +61,17 @@ h5 = animatedline('Color', 'm', 'DisplayName', 'My');
 h6 = animatedline('Color', 'k', 'DisplayName', 'Mz');
 legend show;
 
-
-
 tic;
 AcquisitionTime = 10; 
 desiredSamples = AcquisitionTime * freq;
+
 %% Main Loop
 while true
     pause(0.01)
     t = toc;  
     [err, ~] = calllib('MEGSV86x64', 'GSV86readMultiple', com, int32(0), out, count, valsread, errFlags);
 
-    if valsread.Value > 0
+    if err == 1 && valsread.Value > 0
         validData = out.Value(1:valsread.Value);
         reshaped = reshape(validData, 8, []).';
 
@@ -80,14 +81,12 @@ while true
 
         latestSample = reshaped(end, :);
 
-
         data1 = single(latestSample(1));
         data2 = single(latestSample(2));
         data3 = single(latestSample(3));
         data4 = single(latestSample(4));
         data5 = single(latestSample(5));
         data6 = single(latestSample(6));
-
 
         data1_history = [data1_history; data1];
         data2_history = [data2_history; data2];
@@ -97,7 +96,6 @@ while true
         data6_history = [data6_history; data6];
         time_history = [time_history; t];
 
-
         addpoints(h1, t, data1);
         addpoints(h2, t, data2);
         addpoints(h3, t, data3);
@@ -106,15 +104,20 @@ while true
         addpoints(h6, t, data6);
         drawnow 
 
-
         packet = [data1, data2, data3, data4, data5, data6];
         bytes = typecast(packet, 'uint8');
         write(u, bytes, targetIP, targetPort);
+
+    elseif err == 0
+        disp("No new data available.");
+
+    elseif err == -1
+        calllib('MEGSV86x64', 'GSV86getLastErrorText', com, errTextPtr);
+        disp([' DLL Error: ', errTextPtr.Value]);
+
     end
 end
 
 %% After stop
 calllib('MEGSV86x64', 'GSV86release', com);
 unloadlibrary('MEGSV86x64');
-
-
